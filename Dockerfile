ef9b8ffccf71380846ec77cba75f7c0666ff5f55
K D <diego.unipampa@gmail.com>
2023-07-27 12:04:04 -0300

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM tensorflow/tensorflow:latest
+FROM tensorflow/tensorflow:2.13.0
 RUN apt-get update 
 RUN apt-get -y install vim wget python3 python3-pip
 WORKDIR /droidaugmentor
--- scripts/run_example1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-[ -d outputs ] || { mkdir outputs; }
-pipenv run python generation.py --input_dataset  datasets/drebin215_small_64Malwares_64Benign.csv --data_type float32 --num_samples_class_malware 64 --num_samples_class_benign 64 --number_epochs 1000 --classifier knn --k_fold 5 --latent_dimension 128 --output_dataset outputs/synthetic_dataset_drebin215_small_64Malwares_64Benign.csv 
