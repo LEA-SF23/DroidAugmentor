e5ca74a51389226b352e78123a195a3067443030
Rodrigo Mansilha <rmansilha@users.noreply.github.com>
2023-08-03 10:51:22 -0300

    Update PerceptronModel.py

--- Models/PerceptronModel.py
+++ Models/PerceptronModel.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+__author__ = 'unknown'
+__email__ = 'unknown@unknown.com.br'
+__version__ = '{1}.{0}.{0}'
+__initial_data__ = '2022/06/01'
+__last_update__ = '2023/08/03'
+__credits__ = ['unknown']
+
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import Input
