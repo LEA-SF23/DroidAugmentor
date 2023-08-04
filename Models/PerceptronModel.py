48a81be8136e82c42fe495e64f9a2a335766c326
Rodrigo Mansilha <rmansilha@users.noreply.github.com>
2023-08-03 21:54:07 -0300

    Update PerceptronModel.py

--- Models/PerceptronModel.py
+++ Models/PerceptronModel.py
@@ -40,9 +40,13 @@ class PerceptronMultilayer:
         self.dropout_decay_rate = dropout_decay_rate
 
         if training_metric is None:
-            self.training_metric = DEFAULT_PERCEPTRON_METRIC
+            training_metric = DEFAULT_PERCEPTRON_METRIC
+
         if layers_settings is None:
-            self.layers_settings = DEFAULT_PERCEPTRON_LAYERS_SETTINGS
+            layers_settings = DEFAULT_PERCEPTRON_LAYERS_SETTINGS
+
+        self.training_metric = training_metric
+        self.layers_settings = layers_settings
 
     def get_model(self, input_shape):
 
@@ -52,7 +56,7 @@ class PerceptronMultilayer:
 
         for number_neurons in self.layers_settings[1:]:
             dense_layer = Dense(number_neurons, self.layer_activation)(dense_layer)
-            dense_layer = Dropout(DEFAULT_PERCEPTRON_DROPOUT_DECAY_RATE)(dense_layer)
+            dense_layer = Dropout(self.dropout_decay_rate)(dense_layer)
 
         dense_layer = Dense(1, self.last_layer_activation)(dense_layer)
         neural_network_model = Model(input_layer, dense_layer)
