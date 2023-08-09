bd362520d66d0dc0325ef82c80130945716ce9ab
K D <diego.unipampa@gmail.com>
2023-08-09 11:26:09 -0300

    updates

--- run_demo_app.sh
+++ run_demo_app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pipenv run python main.py --verbosity 20 --output_dir outputs --input_dataset shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
+pipenv run python3 main.py --verbosity 20 --output_dir outputs --input_dataset shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
