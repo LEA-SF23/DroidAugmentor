a8bbded4969f8ad69a58acb80dec9a4ce7dbba64
K D <diego.unipampa@gmail.com>
2023-08-28 20:01:42 -0300

    updates

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /droidaugmentor
 USER root
 RUN rm -rf /droidaugmentor/*
 COPY ./ /droidaugmentor/
-RUN pipenv install -r /droidaugmentor/requirements.txt
+#RUN pipenv install -r /droidaugmentor/requirements.txt
 RUN mv /droidaugmentor/scripts/run_app_in_docker.sh /usr/bin/docker_run.sh
 RUN chmod +x /usr/bin/docker_run.sh 
 CMD ["docker_run.sh"]
--- run_demo_docker.sh
+++ run_demo_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+USER_ID=$(id -u $USER)
 DIR=$(readlink -f shared)
 sudo docker image pull sf23/droidaugmentor:latest
-sudo docker run -it --name=droidaugmentor-$RANDOM -v $DIR:/droidaugmentor/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest /droidaugmentor/shared/app_run.sh --verbosity 20 --output_dir /droidaugmentor/shared/outputs --input_dataset /droidaugmentor/shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
+sudo docker run -it --name=droidaugmentor-$RANDOM -v $DIR:/droidaugmentor/shared -e DISPLAY=unix$DISPLAY sf23/droidaugmentor:latest /droidaugmentor/shared/app_run.sh $USER_ID --verbosity 20 --output_dir /droidaugmentor/shared/outputs --input_dataset /droidaugmentor/shared/datasets/drebin215_small_64Malwares_64Benign.csv --dense_layer_sizes_g 256 --dense_layer_sizes_d 256 --number_epochs 1000 --training_algorithm Adam
 ls shared/outputs/
